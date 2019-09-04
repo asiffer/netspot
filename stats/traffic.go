d922affb8c82b6268b0677eb18eb5a11398d7a65
Alban Siffer <alban.siffer@irisa.fr>
2019-09-04 09:06:10 +0200

    add traffic stat

---
+++ stats/traffic.go
@@ -0,0 +1,60 @@
+// traffic.go
+// TRAFFIC: The ratio #IP packets / window size
+
+package stats
+
+import "math"
+
+func init() {
+	Register("TRAFFIC",
+		func(bs BaseStat) StatInterface {
+			return &Traffic{BaseStat: bs,
+				init:     false,
+				lastTime: 0}
+		})
+}
+
+// Traffic computes the ratio number of IP packets / time window size
+type Traffic struct {
+	BaseStat
+	init     bool
+	lastTime uint64
+}
+
+// Name returns the unique name of the stat
+func (stat *Traffic) Name() string {
+	return "TRAFFIC"
+}
+
+// Requirement returns teh requested counters to compute the stat
+func (stat *Traffic) Requirement() []string {
+	return []string{"IP", "SOURCE_TIME"}
+}
+
+// Compute implements the way to compute the stat from the counters
+func (stat *Traffic) Compute(ctrvalues []uint64) float64 {
+	//ctrvalues[0] -> ip
+	//ctrvalues[1] -> source time
+
+	// if the stat has not stored a starting point yet
+	if !stat.init {
+		// stat.lastPackets = ctrvalues[0]
+		stat.lastTime = ctrvalues[1]
+		stat.init = true
+		return math.NaN()
+	}
+
+	output := 0.
+	// packets is flushed
+	nbPackets := ctrvalues[0]
+	deltaTime := ctrvalues[1] - stat.lastTime
+
+	if deltaTime == 0 || nbPackets == 0 {
+		output = 0.
+	} else {
+		output = float64(nbPackets) / (1e-9 * float64(deltaTime))
+	}
+
+	stat.lastTime = ctrvalues[1]
+	return output
+}
---
+++ stats/traffic_test.go
@@ -0,0 +1,60 @@
+// traffic_test.go
+
+package stats
+
+import (
+	"math"
+	"testing"
+)
+
+func TestTRAFFIC(t *testing.T) {
+	title("Testing TRAFFIC")
+
+	stat := &Traffic{}
+	checkTitle("Checking name...")
+	if stat.Name() != "TRAFFIC" {
+		testERROR()
+		t.Errorf("Expected TRAFFIC, got %s", stat.Name())
+	} else {
+		testOK()
+	}
+
+	checkTitle("Checking requirements...")
+	if !isEqual(stat.Requirement(), []string{"IP", "SOURCE_TIME"}) {
+		testERROR()
+		t.Errorf("Expected [IP, SOURCE_TIME], got %s", stat.Requirement())
+	} else {
+		testOK()
+	}
+
+	checkTitle("Checking computation 1/3...")
+	ctrvalues := []uint64{2, 0}
+	statVal := stat.Compute(ctrvalues)
+	if !math.IsNaN(statVal) {
+		testERROR()
+		t.Errorf("Expected NaN, got %f", statVal)
+	} else {
+		testOK()
+	}
+
+	checkTitle("Checking computation 2/3...")
+	ctrvalues = []uint64{17, 100000000}
+	statVal = stat.Compute(ctrvalues)
+	if statVal != 170. {
+		testERROR()
+		t.Errorf("Expected 170., got %f", statVal)
+	} else {
+		testOK()
+	}
+
+	checkTitle("Checking computation 3/3...")
+	ctrvalues = []uint64{25, 200000000}
+	statVal = stat.Compute(ctrvalues)
+	if statVal != 250. {
+		testERROR()
+		t.Errorf("Expected 250., got %f", statVal)
+	} else {
+		testOK()
+	}
+
+}
