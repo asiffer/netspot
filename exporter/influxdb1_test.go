58642c6adbfb234fe37b76d7baf1b35c8d32c5b5
asr <asr@localhost>
2021-02-06 11:40:29 +0100

    modify config for docker tests

--- exporter/influxdb1_test.go
+++ exporter/influxdb1_test.go
@@ -3,9 +3,28 @@
 package exporter
 
 import (
+	"netspot/config"
+	"os"
 	"testing"
 	"time"
 )
+
+func init() {
+	if isRunningInDockerContainer() {
+		config.SetValue("exporter.influxdb.address", "http://influxdb:8086")
+	}
+}
+
+func isRunningInDockerContainer() bool {
+	// docker creates a .dockerenv file at the root
+	// of the directory tree inside the container.
+	// if this file exists then the viewer is running
+	// from inside a container so return true
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+	return false
+}
 
 func TestInitInflux(t *testing.T) {
 	title(t.Name())
@@ -135,6 +154,4 @@ func TestInfluxStatus(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer Unload(inf.Name())
-
-	// t.Logf("%v+\n", inf.Status())
 }
